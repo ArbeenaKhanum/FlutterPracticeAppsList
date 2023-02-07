f898d7dc19ca4ff34fbd7cd4f5cdafa5edb66251
Arbeena Khanum <arbeenasharpsell.ai@Arbeenas-MacBook-Air.local>
2023-02-07 19:30:38 +0530

    cart and product screens with dismissible, provider & consumer features

--- shop_app/lib/screens/products_overview_screen.dart
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/badge.dart';
 
 import '../providers/cart.dart';
@@ -58,6 +59,7 @@ class _ProductsOveriewScreenState extends State<ProductsOveriewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: new ProductsGridView(_showOnlyFavourites),
     );
   }

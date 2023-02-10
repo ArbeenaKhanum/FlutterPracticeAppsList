e54c2b5c42ff20f9319482e7760e5a186517c30f
Arbeena Khanum <arbeenaak@gmail.com>
2023-02-10 19:22:14 +0530

    https post requests & futures features

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 import 'product.dart';
 
@@ -51,20 +54,31 @@ class Products with ChangeNotifier {
     return _items.where((prodItems) => prodItems.isFavourite).toList();
   }
 
-  void showFavorite() {}
-
-  void showAll() {}
-
-  void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
-    // _items.insert(0, newProduct);
-    notifyListeners();
+  Future<void> addProduct(Product product) {
+    var url = Uri.parse(
+        'https://flutterpracticeshopapp-default-rtdb.firebaseio.com/products.json');
+    return http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'price': product.price,
+              'imageUrl': product.imageUrl,
+              'isFavourite': product.isFavourite
+            }))
+        .then((response) {
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      // _items.insert(0, newProduct);
+      notifyListeners();
+    }).catchError((error) {
+      print(error);
+    });
   }
 
   Product findById(String id) {

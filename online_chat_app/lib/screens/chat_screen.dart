3ee0dad14791ec9dd0086a5bdeb4894447c4f53e
Arbeena Khanum <arbeenaak@gmail.com>
2023-02-22 16:39:01 +0530

    chats screen using firebase

--- online_chat_app/lib/screens/chat_screen.dart
+++ online_chat_app/lib/screens/chat_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chat Screen'),),
+      appBar: AppBar(
+        title: const Text('Chat Screen'),
+      ),
       body: ListView.builder(
         itemBuilder: (ctx, index) => Container(
           padding: const EdgeInsets.all(8),
-          child: const Text('Chats'),
+          child: const Text('Chats Messages'),
         ),
         itemCount: 10,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/u6vbBpbDSxd5XaJ5pWcd/messages')
+              .snapshots()
+              .listen((data) {
+            print('Chat Details: ${data.docs[0]['text']}');
+          });
+        },
       ),
     );
   }
